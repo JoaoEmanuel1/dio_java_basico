7724d5e0c459e05cb4a18260908a37dfb646ffec
Willian-Louback <williandlouback@gmail.com>
2023-08-25 14:42:30 -0300

    Completando o desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem-vindo ao Banco-Dio, insira os seus dados para concluir o cadastro!");
+
+        System.out.println("Por favor, digite o seu nome. ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Certo, " + nomeCliente + ". Agora digite o número da Agência.");
+        int numeroAgencia = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Digite a agência.");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Qual o seu saldo?");
+        double saldo = Double.valueOf(scanner.nextLine());
+
+        scanner.close();
+
+        System.out.println(
+            "Olá " + nomeCliente +
+            ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+            ", conta " + numeroAgencia +
+            " e seu saldo " + saldo +
+            " já está disponível para saque!"
+        );
     }
 }
