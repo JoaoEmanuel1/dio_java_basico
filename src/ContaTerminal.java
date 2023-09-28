3af4bf87e9b9c6397613de87623df756a0324c8d
JoaoEmanuel <joaoemanuelcf01@gmail.com>
2023-09-28 16:39:41 -0300

    second commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,26 +7,50 @@ public class ContaTerminal {
 
         System.out.println("Bem-vindo ao Banco-Dio, insira os seus dados para concluir o cadastro!");
 
+        // Solicitar e ler o nome do cliente
         System.out.println("Por favor, digite o seu nome. ");
         String nomeCliente = scanner.nextLine();
 
+        // Solicitar e ler o número da Agência
         System.out.println("Certo, " + nomeCliente + ". Agora digite o número da Agência.");
-        int numeroAgencia = Integer.valueOf(scanner.nextLine());
+        int numeroAgencia = 0;
+        boolean numeroValido = false;
+        while (!numeroValido) {
+            try {
+                numeroAgencia = Integer.parseInt(scanner.nextLine());
+                numeroValido = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Número da Agência inválido. Digite novamente.");
+            }
+        }
 
-        System.out.println("Digite a agência.");
-        String agencia = scanner.nextLine();
+        // Solicitar e ler o número da conta
+        System.out.println("Digite o número da conta:");
+        String numeroConta = scanner.nextLine();
 
+        // Solicitar e ler o saldo da conta
         System.out.println("Qual o seu saldo?");
-        double saldo = Double.valueOf(scanner.nextLine());
+        double saldo = 0.0;
+        boolean saldoValido = false;
+        while (!saldoValido) {
+            try {
+                saldo = Double.parseDouble(scanner.nextLine());
+                saldoValido = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Saldo inválido. Digite novamente.");
+            }
+        }
 
+        // Fechar o scanner
         scanner.close();
 
+        // Exibir os detalhes da conta
         System.out.println(
             "Olá " + nomeCliente +
-            ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
-            ", conta " + numeroAgencia +
-            " e seu saldo " + saldo +
-            " já está disponível para saque!"
+            ", obrigado por criar uma conta em nosso banco. Sua agência é " + numeroAgencia +
+            ", conta " + numeroConta +
+            " e seu saldo é " + saldo +
+            ". Seu saldo já está disponível para saque!"
         );
     }
 }
